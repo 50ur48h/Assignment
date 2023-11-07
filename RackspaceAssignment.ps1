bae9ad328d5c6b7ac274979722949ece57c3a345
Sourabh Kumrawat <sourabhkumrawat00@gmail.com>
2023-11-07 10:41:04 +0530

    Create RackspaceAssignment.ps1

---
+++ RackspaceAssignment.ps1
@@ -0,0 +1,35 @@
+function Get-CommonTimeZone {
+    [CmdletBinding()]
+    param(
+        [string]$Name,
+        [int]$Offset
+    )
+
+    if ($Name -ne $null -and $Offset -ne 0) {
+        Write-Host "Please provide only one parameter: 'Name' or 'Offset'."
+        return
+    }
+
+    if ($Offset -ne 0 -and ($Offset -lt -12 -or $Offset -gt 12)) {
+        Write-Error "Offset value should be between -12 and 12."
+        return
+    }
+
+    $timezone = "https://raw.githubusercontent.com/dmfilipenko/timezones.json/master/timezones.json"
+
+    try {
+        $timeZoneData = Invoke-RestMethod -Uri $timezone -UseBasicParsing
+    } catch {
+        Write-Host "Failed to download time zone data from Github."
+    }
+
+    if ($Name -ne $null) {
+        $filteredTimeZones = $timeZoneData | Where-Object { $_.utc -like "*$Name*"}
+    } elseif ($Offset -ne 0) {
+        $filteredTimeZones = $timeZoneData | Where-Object { $_.offset -eq $Offset }
+    } else {
+        $filteredTimeZones = $timeZoneData
+    }
+
+    $filteredTimeZones | FT
+}
